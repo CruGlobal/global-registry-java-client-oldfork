9d4f00eccb9ae1d18b2bec27d37fb0372236b5c8
Ryan Carlson <ryan.t.carlson@cru.org>
2014-02-23 10:11:42 -0500

    Create initial endpoint classes for the `EntityEndpoints`

--- src/main/java/org/cru/globalreg/client/EntityEndpoints.java
+++ src/main/java/org/cru/globalreg/client/EntityEndpoints.java
@@ -1,7 +1,14 @@
 package org.cru.globalreg.client;
 
+import org.codehaus.jackson.JsonNode;
+
 /**
  * Created by ryancarlson on 2/23/14.
  */
-public interface EntityEndpoints {
+public interface EntityEndpoints
+{
+    JsonNode get(String... criteria);
+    void create(JsonNode entity);
+    void update(JsonNode entity);
+    void delete(JsonNode entity);
 }
--- src/main/java/org/cru/globalreg/client/impl/EntityEndpointsImpl.java
+++ src/main/java/org/cru/globalreg/client/impl/EntityEndpointsImpl.java
@@ -1,7 +1,30 @@
 package org.cru.globalreg.client.impl;
 
+import org.codehaus.jackson.JsonNode;
+import org.cru.globalreg.client.EntityEndpoints;
+
 /**
  * Created by ryancarlson on 2/23/14.
  */
-public class EntityEndpointsImpl {
+public class EntityEndpointsImpl implements EntityEndpoints
+{
+    @Override
+    public JsonNode get(String... criteria) {
+        return null;
+    }
+
+    @Override
+    public void create(JsonNode entity) {
+
+    }
+
+    @Override
+    public void update(JsonNode entity) {
+
+    }
+
+    @Override
+    public void delete(JsonNode entity) {
+
+    }
 }
