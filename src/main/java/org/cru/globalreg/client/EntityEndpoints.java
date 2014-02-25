221a77ed8fe15259bff283286cef23502f7d9183
Daniel Frett <daniel.frett@ccci.org>
2014-02-25 11:54:08 +0800

    implement entity search functionality

--- src/main/java/org/cru/globalreg/client/EntityEndpoints.java
+++ src/main/java/org/cru/globalreg/client/EntityEndpoints.java
@@ -7,6 +7,8 @@ import org.codehaus.jackson.JsonNode;
  */
 public interface EntityEndpoints
 {
+    JsonNode search(String type, Filter... filters);
+    JsonNode search(String type, int page, Filter... filters);
     JsonNode get(Integer id, String type);
     JsonNode create(JsonNode entity, String type);
     JsonNode update(Integer id, JsonNode entity, String type);
